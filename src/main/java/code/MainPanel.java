3578856bcfab9bfe93878c31adfd46a102fbf255
passivegoose <boris.mikhail09200@gmail.com>
2023-12-01 13:01:31 +0300

    adding javafx example

--- src/main/java/code/MainPanel.java
+++ src/main/java/code/MainPanel.java
@@ -3,6 +3,9 @@ package code;
 import code.calculation.CubeController;
 import code.calculation.CubesModel;
 import javafx.application.Application;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Box;
 import javafx.stage.Stage;
 
 public final class MainPanel extends Application {
@@ -10,6 +13,10 @@ public final class MainPanel extends Application {
     @Override
     public void start(Stage stage) {
         buildCubeModel();
+
+        Box box = new Box(5, 5, 5);
+        PhongMaterial material = new PhongMaterial(Color.RED);
+        box.setMaterial(material);
     }
 
     public static void main(String[] args) {
